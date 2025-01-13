348b1aa7a3671c492981ef72327d183500d852e5
lumiscosity <averyrudelphe@gmail.com>
2025-01-13 21:04:41 +0100

    cleanup

--- src/main/java/com/lumiscosity/aa4atlas/AA4Atlas.java
+++ src/main/java/com/lumiscosity/aa4atlas/AA4Atlas.java
@@ -1,27 +1,17 @@
 package com.lumiscosity.aa4atlas;
 
-import folk.sisby.surveyor.Surveyor;
-import folk.sisby.surveyor.config.SurveyorConfig;
-import folk.sisby.surveyor.config.SystemMode;
 import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class AA4Atlas implements ModInitializer {
 	public static final String MOD_ID = "aa4-atlas";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
-	public static final SurveyorConfig BACKUP_SURVEYOR_CONFIG = (SurveyorConfig)SurveyorConfig.createToml(FabricLoader.getInstance().getConfigDir(), "", "aa4-atlas-svbackup", SurveyorConfig.class);
-	public static boolean RELOAD = false;
 
 	public static final Item ANTIQUE_ATLAS = new AtlasItem(new Item.Settings().maxCount(1));
 
