fc8e73d60832fca88c330eef4ddf72d908d02621
lumiscosity <averyrudelphe@gmail.com>
2025-07-31 21:46:17 +0200

    Fix server-side dependency resolution

--- src/client/java/com/lumiscosity/aa4atlas/AA4AtlasClient.java
+++ src/client/java/com/lumiscosity/aa4atlas/AA4AtlasClient.java
@@ -3,12 +3,14 @@ package com.lumiscosity.aa4atlas;
 import folk.sisby.antique_atlas.gui.AtlasScreen;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import net.fabricmc.loader.api.FabricLoader;
 
 public class AA4AtlasClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
+		if (!FabricLoader.getInstance().isModLoaded("antique_atlas")) {
+			throw new RuntimeException("Antique Atlas 4 is required for AA4 Atlas to function, but it wasn't found!");
+		}
 		ClientPlayNetworking.registerGlobalReceiver(AtlasOpenPayload.ID, (payload, context) -> {
 			context.client().execute(() -> {
 				if (context.client().currentScreen == null) {
